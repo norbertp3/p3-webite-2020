b3d955808acb3c7b0c3bba7d286d7288fba3266b
Norbert <norbert@p3.ie>
2020-03-05 14:55:36 +0000

    wip

--- public/js/custom.js
+++ public/js/custom.js
@@ -7,5 +7,11 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
-// Home Banner
+// Read more
+$('.expand').find('a[href="#"]').on('click', function (e) {
+    e.preventDefault();
+    this.expand = !this.expand;
+    $(this).text(this.expand?"Read Less...":"Read More...");
+    $(this).closest('.expand').find('.hidden-list').slideToggle();
+});
 
